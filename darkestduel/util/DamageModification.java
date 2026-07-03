0dfff33d0c1d1a1f786d5d79bd8ba5e72b05563c
zBcxz <marquessr06s@gmail.com>
2026-07-03 22:11:28 +0000

    util updating

---
+++ darkestduel/util/DamageModification.java
@@ -0,0 +1,17 @@
+static class DamageModification {
+        private final int damage;
+        private final List<String> messages;
+
+        public DamageModification(int damage, List<String> messages) {
+            this.damage = damage;
+            this.messages = messages;
+        }
+
+        public int getDamage() {
+            return damage;
+        }
+
+        public List<String> getMessages() {
+            return messages;
+        }
+    }
